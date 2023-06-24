94aa16a3a56d9d2c1ba525bf550d537ad422b9e7
Sayantan-Bhowmick <sayantanbhowmick27523@gmail.com>
2023-06-24 14:10:36 +0000

    Clear workspace

--- springapp/src/main/java/com/ae_crms_t18/crms/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ae_crms_t18.crms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ae_crms_t18.crms.model.Ticket;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
