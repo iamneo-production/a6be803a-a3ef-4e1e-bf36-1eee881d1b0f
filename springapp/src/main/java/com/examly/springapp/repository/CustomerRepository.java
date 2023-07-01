5f09b0860f22e8ccb11f49ff68a8d80da12e45d5
Sayantan-Bhowmick <sayantanbhowmick27523@gmail.com>
2023-07-01 12:50:42 +0000

    Minor Changes

--- springapp/src/main/java/com/examly/springapp/repository/CustomerRepository.java
+++
@@ -1,4 +0,0 @@
-package com.examly.springapp.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.examly.springapp.model.Customer;
-public interface CustomerRepository extends JpaRepository<Customer, Long> {}
