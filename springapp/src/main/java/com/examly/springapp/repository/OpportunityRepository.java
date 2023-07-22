ae38c48d803d758862f2eff0176651de04913c11
koena7 <koena2204@gmail.com>
2023-07-22 15:50:45 +0000

    minor

--- springapp/src/main/java/com/examly/springapp/repository/OpportunityRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/OpportunityRepository.java
@@ -2,6 +2,7 @@ package com.examly.springapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.examly.springapp.model.Opportunity;
+import com.examly.springapp.enumeration.OpportunityStatus;
 
 public interface OpportunityRepository extends JpaRepository<Opportunity,Long>{
     long countByStatus(OpportunityStatus status);
--- springapp/src/main/java/com/examly/springapp/repository/SaleRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/SaleRepository.java
@@ -4,6 +4,8 @@ import com.examly.springapp.model.Sale;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+
 public interface SaleRepository extends JpaRepository<Sale,Long> {
     List<Sale> findByCustomerId(Long customerId);
 
