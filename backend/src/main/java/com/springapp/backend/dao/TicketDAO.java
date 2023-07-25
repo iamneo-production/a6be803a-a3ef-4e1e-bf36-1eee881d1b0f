6f6b10dc150e5f0dab8a640a9a46adde6b78c981
vasanth-123 <vasanthpanner2002@gmail.com>
2023-07-25 19:34:50 +0000

    Testcase success

--- backend/src/main/java/com/springapp/backend/dao/TicketDAO.java
+++
@@ -1,9 +0,0 @@
-package com.springapp.backend.dao;
-import java.util.List;
-import com.springapp.backend.model.Ticket;
-public interface TicketDAO {
-    List<Ticket> getAllTickets();
-    Ticket saveTicket(Ticket ticket);
-    Ticket getTicketById(Long id);
-    void deleteTicket(Ticket ticket);
-}
