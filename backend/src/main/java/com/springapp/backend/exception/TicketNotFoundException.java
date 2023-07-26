6340f283c7e0525c6bd6e02782b8c2aff4087014
Sowndarya-nukala3 <sowndaryanukala@gmail.com>
2023-07-26 20:54:59 +0000

    test cases done

--- backend/src/main/java/com/springapp/backend/exception/TicketNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.springapp.backend.exception;
-public class TicketNotFoundException extends RuntimeException {
-    public TicketNotFoundException(String message) {
-        super(message);
-    }
-    public TicketNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
