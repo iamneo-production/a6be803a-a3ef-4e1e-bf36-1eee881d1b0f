91b82d7f090c7b0f2f52d8d85a5f6e900686e40f
vasanth-123 <vasanthpanner2002@gmail.com>
2023-07-26 08:20:18 +0000

    query deleted

--- springapp/src/main/java/com/examly/springapp/repository/SaleRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/SaleRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import org.springframework.data.jpa.repository.Query;
+
 
 public interface SaleRepository extends JpaRepository<Sale,Long> {
     List<Sale> findByCustomerId(Long customerId);
