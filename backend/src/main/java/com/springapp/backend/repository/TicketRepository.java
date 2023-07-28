b1f3a02d515ef08d925546652cd8082079c05c01
Sayantan-Bhowmick <sayantanbhowmick27523@gmail.com>
2023-07-28 16:31:45 +0000

    cleanup

--- backend/src/main/java/com/springapp/backend/repository/TicketRepository.java
+++
@@ -1,4 +0,0 @@
-package com.springapp.backend.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.springapp.backend.model.Ticket;
-public interface TicketRepository extends JpaRepository<Ticket, Long> {}
